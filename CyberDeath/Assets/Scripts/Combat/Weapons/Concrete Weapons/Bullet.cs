776941fe87922d7ac9446599b333293a5760b27c
John Mordi <johnmordi00@gmail.com>
2022-03-16 20:24:22 -0500

    Bullet go towards player now

--- CyberDeath/Assets/Scripts/Combat/Weapons/Concrete Weapons/Bullet.cs
+++ CyberDeath/Assets/Scripts/Combat/Weapons/Concrete Weapons/Bullet.cs
@@ -8,18 +8,18 @@ namespace GoofyGhosts
     {
 
         public Transform target;
-        Vector3 newDirection;
+        Vector3 newDirection, targetDirection;
 
         // Start is called before the first frame update
         void Start()
         {
             target = GameObject.FindGameObjectWithTag("Player").transform;
-            Vector3 targetPosition = target.position + new Vector3(0, 2.27f, 0);
+            Vector3 targetPosition = target.position + new Vector3(0, 1.8f, 0);
             //gameObject.transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, GameObject.Find("Player").transform.position - transform.position, 1, 1));
 
             //StartCoroutine(EndBullet());
             // Determine which direction to rotate towards
-            Vector3 targetDirection = targetPosition - transform.position;
+            targetDirection = (targetPosition - transform.position).normalized * 5;
 
             // The step size is equal to speed times frame time.
             float singleStep = 2;
@@ -28,7 +28,7 @@ namespace GoofyGhosts
             newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
 
             // Draw a ray pointing at our target in
-            Debug.DrawRay(transform.position, newDirection, Color.red);
+            //Debug.DrawRay(transform.position, newDirection, Color.red);
 
             // Calculate a rotation a step closer to the target and applies rotation to this object
             //transform.rotation = Quaternion.LookRotation(newDirection);
@@ -41,7 +41,7 @@ namespace GoofyGhosts
         {
             Debug.DrawRay(transform.position, transform.forward, Color.red);
             Debug.DrawRay(transform.position, newDirection, Color.green);
-            transform.Translate(newDirection * 5 * Time.deltaTime);
+            transform.Translate(new Vector3(targetDirection.x, targetDirection.y, targetDirection.z) * Time.deltaTime);
         }
 
         private void OnTriggerEnter(Collider other)
