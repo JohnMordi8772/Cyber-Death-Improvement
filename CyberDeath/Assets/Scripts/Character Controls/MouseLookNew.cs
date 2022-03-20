ac73571f464d24794a6092cd24ad48065ee4e55d
John Mordi <johnmordi00@gmail.com>
2022-03-20 13:00:54 -0500

    Updated mouse look so that it works in all screen sizes

--- CyberDeath/Assets/Scripts/Character Controls/MouseLookNew.cs
+++ CyberDeath/Assets/Scripts/Character Controls/MouseLookNew.cs
@@ -9,8 +9,9 @@ namespace GoofyGhosts
 {
     public class MouseLookNew : MonoBehaviour
     {
-        private Vector3 mousePos;
+        private Vector2 mousePos;
         private Quaternion newRot;
+        Vector2 screenCenter;
 
         private PlayerControls newPC;
         public static bool scriptEnabled;
@@ -20,20 +21,28 @@ namespace GoofyGhosts
         {
             newPC = new PlayerControls();
             scriptEnabled = true;
+            Cursor.lockState = CursorLockMode.Locked;
+            
+            Cursor.lockState = CursorLockMode.Confined;
+            
         }
 
         // Update is called once per frame
         void Update()
         {
+            screenCenter = new Vector2(Screen.width / 2, Screen.height / 2);
             mousePos = Mouse.current.position.ReadValue();
-            mousePos.z = mousePos.y;
-            mousePos.y = 0;
-            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            Vector2 direction = (mousePos - screenCenter).normalized;
+            float angle = (Mathf.Atan2(-direction.y, direction.x)) * Mathf.Rad2Deg;
+            print(direction);
+            transform.rotation = Quaternion.Euler(0, angle+90, 0);
+            ////mousePos.z = mousePos.y;
+            ////mousePos.y = 0;
+            //mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            //mousePos.z = -(mousePos.y + 330);
+            //mousePos.y = transform.position.y;
             //print(mousePos);
-            mousePos.z = -(mousePos.y + 330);
-            mousePos.y = transform.position.y;
-            print(mousePos);
-            transform.LookAt(mousePos);
+            //transform.LookAt(mousePos);
 
             /*
             newRot = transform.rotation;
