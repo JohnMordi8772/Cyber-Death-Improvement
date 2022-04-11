e2f6db26660f79207337345da51f32fb862512b5
John Mordi <johnmordi00@gmail.com>
2022-04-11 10:06:48 -0500

    Some of the last updates

--- CyberDeath/Assets/Shop/Scripts/NewUpgrades.cs
+++ CyberDeath/Assets/Shop/Scripts/NewUpgrades.cs
@@ -193,12 +193,14 @@ namespace GoofyGhosts
 
         public void GroundSlam()
         {
-            if (aCounter1 == 2 && GetScrapCount() >= 25 && !pm.purchase2)
+            if (aCounter1 == 2 && GetScrapCount() >= 25 && ScrapCounter.wireCount >= 5 && ScrapCounter.fanCount >= 5 && !pm.purchase2)
             {
                 am.slamPurchased = true;
                 aCounter1++;
                 pm.purchase2 = true;
                 ScrapCounter.scrapCount -= 25;
+                ScrapCounter.wireCount -= 5;
+                ScrapCounter.fanCount -= 5;
                 ScrapCounter.OnScrapCountChange();
                 UpdateMoneyText();
                 buttons[1].image.color = Color.white;
@@ -212,11 +214,14 @@ namespace GoofyGhosts
 
         public void EMP()
         {
-            if ((aCounter1 == 3 || dCounter1 == 3) && GetScrapCount() >= 25 && !pm.purchase18)
+            if ((aCounter1 == 3 || dCounter1 == 3) && GetScrapCount() >= 25 && ScrapCounter.wireCount >= 5 && ScrapCounter.fanCount >= 5 && ScrapCounter.coilCount >= 5 && !pm.purchase18)
             {
                 am.empPurchased = true;
                 pm.purchase18 = true;
                 ScrapCounter.scrapCount -= 25;
+                ScrapCounter.wireCount -= 5;
+                ScrapCounter.fanCount -= 5;
+                ScrapCounter.coilCount -= 5;
                 ScrapCounter.OnScrapCountChange();
                 UpdateMoneyText();
                 buttons[15].interactable = false;
@@ -226,12 +231,15 @@ namespace GoofyGhosts
 
         public void AttackPowerUp1()
         {
-            if (aCounter1 == 3 && aCounter2 == 0 && GetScrapCount() >= 40 && !pm.purchase3)
+            if (aCounter1 == 3 && aCounter2 == 0 && GetScrapCount() >= 40 && ScrapCounter.wireCount >= 5 && ScrapCounter.fanCount >= 5 && ScrapCounter.coilCount >= 5 && !pm.purchase3)
             {
                 playerAttack.OnPurchased(10, 2);
                 aCounter2++;
                 pm.purchase3 = true;
                 ScrapCounter.scrapCount -= 40;
+                ScrapCounter.wireCount -= 5;
+                ScrapCounter.fanCount -= 5;
+                ScrapCounter.coilCount -= 5;
                 ScrapCounter.OnScrapCountChange();
                 UpdateMoneyText();
                 buttons[2].image.color = Color.white;
@@ -242,12 +250,15 @@ namespace GoofyGhosts
 
         public void AttackPowerUp2()
         {
-            if (aCounter2 == 1 && GetScrapCount() >= 50 && !pm.purchase4)
+            if (aCounter2 == 1 && GetScrapCount() >= 50 && ScrapCounter.wireCount >= 10 && ScrapCounter.fanCount >= 10 && ScrapCounter.coilCount >= 5 && !pm.purchase4)
             {
                 playerAttack.OnPurchased(20, 4);
                 aCounter2++;
                 pm.purchase4 = true;
                 ScrapCounter.scrapCount -= 50;
+                ScrapCounter.wireCount -= 10;
+                ScrapCounter.fanCount -= 10;
+                ScrapCounter.coilCount -= 5;
                 ScrapCounter.OnScrapCountChange();
                 UpdateMoneyText();
                 buttons[2].interactable = false;
@@ -257,12 +268,15 @@ namespace GoofyGhosts
 
         public void AttackSpeedUp1()
         {
-            if (aCounter1 == 3 && aCounter3 == 0 && GetScrapCount() >= 40 && !pm.purchase5)
+            if (aCounter1 == 3 && aCounter3 == 0 && GetScrapCount() >= 40 && ScrapCounter.wireCount >= 5 && ScrapCounter.fanCount >= 5 && ScrapCounter.coilCount >= 5 && !pm.purchase5)
             {
                 playerSwing.OnPurchased(0.10f, 2);
                 aCounter3++;
                 pm.purchase5 = true;
                 ScrapCounter.scrapCount -= 40;
+                ScrapCounter.wireCount -= 5;
+                ScrapCounter.fanCount -= 5;
+                ScrapCounter.coilCount -= 5;
                 ScrapCounter.OnScrapCountChange();
                 UpdateMoneyText();
                 buttons[4].image.color = Color.white;
@@ -273,12 +287,15 @@ namespace GoofyGhosts
 
         public void AttackSpeedUp2()
         {
-            if (aCounter3 == 1 && GetScrapCount() >= 50 && !pm.purchase6)
+            if (aCounter3 == 1 && GetScrapCount() >= 50 && ScrapCounter.wireCount >= 10 && ScrapCounter.fanCount >= 10 && ScrapCounter.coilCount >= 5 && !pm.purchase6)
             {
                 playerSwing.OnPurchased(0.20f, 4);
                 aCounter3++;
                 pm.purchase6 = true;
                 ScrapCounter.scrapCount -= 50;
+                ScrapCounter.wireCount -= 10;
+                ScrapCounter.fanCount -= 10;
+                ScrapCounter.coilCount -= 5;
                 ScrapCounter.OnScrapCountChange();
                 UpdateMoneyText();
                 buttons[4].interactable = false;
@@ -288,12 +305,15 @@ namespace GoofyGhosts
 
         public void CritChanceUp1()
         {
-            if (aCounter1 == 3 && aCounter4 == 0 && GetScrapCount() >= 40 && !pm.purchase7)
+            if (aCounter1 == 3 && aCounter4 == 0 && GetScrapCount() >= 40 && ScrapCounter.wireCount >= 5 && ScrapCounter.fanCount >= 5 && ScrapCounter.coilCount >= 5 && !pm.purchase7)
             {
                 GameObject.Find("CritChanceStorage").GetComponent<CritChanceStorage>().critChance += 10;
                 aCounter4++;
                 pm.purchase7 = true;
                 ScrapCounter.scrapCount -= 40;
+                ScrapCounter.wireCount -= 5;
+                ScrapCounter.fanCount -= 5;
+                ScrapCounter.coilCount -= 5;
                 ScrapCounter.OnScrapCountChange();
                 UpdateMoneyText();
                 buttons[13].image.color = Color.white;
@@ -304,12 +324,15 @@ namespace GoofyGhosts
 
         public void CritChanceUp2()
         {
-            if (aCounter4 == 1 && GetScrapCount() >= 50 && !pm.purchase8)
+            if (aCounter4 == 1 && GetScrapCount() >= 50 && ScrapCounter.wireCount >= 10 && ScrapCounter.fanCount >= 10 && ScrapCounter.coilCount >= 5 && !pm.purchase8)
             {
                 GameObject.Find("CritChanceStorage").GetComponent<CritChanceStorage>().critChance += 15;
                 aCounter4++;
                 pm.purchase8 = true;
                 ScrapCounter.scrapCount -= 50;
+                ScrapCounter.wireCount -= 10;
+                ScrapCounter.fanCount -= 10;
+                ScrapCounter.coilCount -= 5;
                 ScrapCounter.OnScrapCountChange();
                 UpdateMoneyText();
                 buttons[13].interactable = false;
@@ -355,12 +378,14 @@ namespace GoofyGhosts
 
         public void TaniumSkin()
         {
-            if (dCounter1 == 2 && GetScrapCount() >= 25 && !pm.purchase10)
+            if (dCounter1 == 2 && GetScrapCount() >= 25 && ScrapCounter.wireCount >= 5 && ScrapCounter.fanCount >= 5 && !pm.purchase10)
             {
                 am.taniumPurchased = true;
                 dCounter1++;
                 pm.purchase10 = true;
                 ScrapCounter.scrapCount -= 25;
+                ScrapCounter.wireCount -= 5;
+                ScrapCounter.fanCount -= 5;
                 ScrapCounter.OnScrapCountChange();
                 UpdateMoneyText();
                 buttons[6].image.color = Color.white;
@@ -374,12 +399,15 @@ namespace GoofyGhosts
 
         public void HealthUp1()
         {
-            if (dCounter1 == 3 && dCounter2 == 0 && GetScrapCount() >= 40 && !pm.purchase11) 
+            if (dCounter1 == 3 && dCounter2 == 0 && GetScrapCount() >= 40 && ScrapCounter.wireCount >= 5 && ScrapCounter.fanCount >= 5 && ScrapCounter.armorCount >= 5 && !pm.purchase11) 
             { 
                 playerHealth.OnPurchased(10, 2);
                 dCounter2++;
                 pm.purchase11 = true;
                 ScrapCounter.scrapCount -= 40;
+                ScrapCounter.wireCount -= 5;
+                ScrapCounter.fanCount -= 5;
+                ScrapCounter.armorCount -= 5;
                 ScrapCounter.OnScrapCountChange();
                 UpdateMoneyText();
                 buttons[7].image.color = Color.white;
@@ -390,12 +418,15 @@ namespace GoofyGhosts
 
         public void HealthUp2()
         {
-            if (dCounter2 == 1 && GetScrapCount() >= 50 && !pm.purchase12)
+            if (dCounter2 == 1 && GetScrapCount() >= 50 && ScrapCounter.wireCount >= 10 && ScrapCounter.fanCount >= 10 && ScrapCounter.armorCount >= 5 && !pm.purchase12)
             {
                 playerHealth.OnPurchased(10, 2);
                 dCounter2++;
                 pm.purchase12 = true;
                 ScrapCounter.scrapCount -= 50;
+                ScrapCounter.wireCount -= 10;
+                ScrapCounter.fanCount -= 10;
+                ScrapCounter.armorCount -= 5;
                 ScrapCounter.OnScrapCountChange();
                 UpdateMoneyText();
                 buttons[7].interactable = false;
@@ -405,12 +436,15 @@ namespace GoofyGhosts
 
         public void ArmorUp1()
         {
-            if (dCounter1 == 3 && dCounter3 == 0 && GetScrapCount() >= 40 && !pm.purchase13)
+            if (dCounter1 == 3 && dCounter3 == 0 && GetScrapCount() >= 40 && ScrapCounter.wireCount >= 5 && ScrapCounter.fanCount >= 5 && ScrapCounter.armorCount >= 5 && !pm.purchase13)
             {
                 playerArmor.OnPurchased(10, 2);
                 dCounter3++;
                 pm.purchase13 = true;
                 ScrapCounter.scrapCount -= 40;
+                ScrapCounter.wireCount -= 5;
+                ScrapCounter.fanCount -= 5;
+                ScrapCounter.armorCount -= 5;
                 ScrapCounter.OnScrapCountChange();
                 UpdateMoneyText();
                 buttons[9].image.color = Color.white;
@@ -421,12 +455,15 @@ namespace GoofyGhosts
 
         public void ArmorUp2()
         {
-            if (dCounter3 == 1 && GetScrapCount() >= 50 && !pm.purchase14)
+            if (dCounter3 == 1 && GetScrapCount() >= 50 && ScrapCounter.wireCount >= 10 && ScrapCounter.fanCount >= 10 && ScrapCounter.armorCount >= 5 && !pm.purchase14)
             {
                 playerArmor.OnPurchased(10, 2);
                 dCounter3++;
                 pm.purchase14 = true;
                 ScrapCounter.scrapCount -= 50;
+                ScrapCounter.wireCount -= 10;
+                ScrapCounter.fanCount -= 10;
+                ScrapCounter.armorCount -= 5;
                 ScrapCounter.OnScrapCountChange();
                 UpdateMoneyText();
                 buttons[9].interactable = false;
@@ -436,12 +473,15 @@ namespace GoofyGhosts
 
         public void MoveSpeedUp1()
         {
-            if (dCounter1 == 3 && dCounter4 == 0 && GetScrapCount() >= 40 && !pm.purchase15)
+            if (dCounter1 == 3 && dCounter4 == 0 && GetScrapCount() >= 40 && ScrapCounter.wireCount >= 5 && ScrapCounter.fanCount >= 5 && ScrapCounter.armorCount >= 5 && !pm.purchase15)
             {
                 playerMove.OnPurchased(1, 2);
                 dCounter4++;
                 pm.purchase15 = true;
                 ScrapCounter.scrapCount -= 40;
+                ScrapCounter.wireCount -= 5;
+                ScrapCounter.fanCount -= 5;
+                ScrapCounter.armorCount -= 5;
                 ScrapCounter.OnScrapCountChange();
                 UpdateMoneyText();
                 buttons[11].image.color = Color.white;
@@ -452,12 +492,15 @@ namespace GoofyGhosts
 
         public void MoveSpeedUp2()
         {
-            if (dCounter4 == 1 && GetScrapCount() >= 50 && !pm.purchase16)
+            if (dCounter4 == 1 && GetScrapCount() >= 50 && ScrapCounter.wireCount >= 10 && ScrapCounter.fanCount >= 10 && ScrapCounter.armorCount >= 5 && !pm.purchase16)
             {
                 playerMove.OnPurchased(1, 2);
                 dCounter4++;
                 pm.purchase16 = true;
                 ScrapCounter.scrapCount -= 50;
+                ScrapCounter.wireCount -= 10;
+                ScrapCounter.fanCount -= 10;
+                ScrapCounter.armorCount -= 5;
                 ScrapCounter.OnScrapCountChange();
                 UpdateMoneyText();
                 buttons[11].interactable = false;
